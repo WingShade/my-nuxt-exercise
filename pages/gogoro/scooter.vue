4670c0e4bc77404617de9b0477d50ffd7e7cf57d
Neil <unicornwing213@gmail.com>
2020-04-22 16:07:07 +0800

    set prettier

---
+++ pages/gogoro/scooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Smartscooter®</div>
+</template>
+
+<script>
+export default {
+  name: 'smartScooter'
+}
+</script>
+
+<style scoped></style>
---
+++ pages/gogoro/shopAddress.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    尋找門市
+    <br />
+    <br />
+    請輸入你要找的門市(限數字)：<input type="text" v-model="where" />
+    <br />
+    <br />
+    (Validate Route Params)
+    <br />
+    <br />
+    <input type="button" value="送出" @click="find" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'shopAddress',
+  data() {
+    return {
+      where: ''
+    }
+  },
+  methods: {
+    find() {
+      this.$router.push(`/gogoro/findShop/${this.where}`)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
